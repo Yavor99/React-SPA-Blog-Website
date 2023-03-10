b1cee32816782fb2887ebda14a04ef6577cfcbe3
Yavor99 <yavor_enchev99@abv.bg>
2023-03-10 21:15:49 +0200

    Add register page and add routes

--- src/App.js
+++ src/App.js
@@ -1,15 +1,29 @@
 import Home from "./components/pages/home/Home";
 import Login from "./components/pages/login/Login";
+import Register from "./components/pages/register/Register";
 import Settings from "./components/pages/settings/Settings";
 import Single from "./components/pages/single/Single";
 import Write from "./components/pages/write/Write";
 import TopBar from "./components/topBar/TopBar";
+import {
+  Routes,
+  Route
+} from "react-router-dom";
 
 function App() {
+  const user = false;
   return (
     <>
-      <TopBar />
-      <Login />
+      <TopBar /> 
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/settings" element={user ? <Settings /> : <Login />} />
+        <Route path="/login" element={user ? <Home/> : <Login />} />
+        <Route path="/register" element={user ? <Home/> : <Register/>} />        
+        <Route path="/write" element={user ? <Write /> : <Login />} />
+        <Route path="/post/:postId" element={<Single />} />
+      </Routes>
+      
     </>
   );
 }
--- src/components/topBar/TopBar.js
+++ src/components/topBar/TopBar.js
@@ -1,6 +1,8 @@
 import "./topbar.css"
+import { Link } from "react-router-dom"
 
 export default function TopBar() {
+    const user = false;
   return (
     <div className="top">
         <div className="topLeft">
@@ -13,19 +15,47 @@ export default function TopBar() {
         
         <div className="topCenter">
             <ul className="topList">
-                <li className="topListItem">HOME</li>
-                <li className="topListItem">ABOUT</li>
-                <li className="topListItem">CONTANCT</li>
-                <li className="topListItem">WRITE</li>
-                <li className="topListItem">LOGOUT</li>
+                <li className="topListItem">
+                    <Link className="link" to="/">HOME</Link>
+                </li>
+                <li className="topListItem">
+                    <Link className="link" to="/about">ABOUT</Link>
+                </li>
+                <li className="topListItem">
+                    <Link className="link" to="/contact">CONTANCT</Link>
+                </li>
+                <li className="topListItem">
+                    <Link className="link" to="/write">WRITE</Link>
+                </li>
+                <li className="topListItem">
+                    {user && "LOGOUT"}
+                </li>
             </ul>
         </div>
         <div className="topRight">
-            <img 
-                className="topImg"
-                src="https://cdn.hswstatic.com/gif/play/0b7f4e9b-f59c-4024-9f06-b3dc12850ab7-1920-1080.jpg"
-                alt=""    
-            />
+            {
+                user ? (
+                    <img 
+                        className="topImg"
+                        src="https://cdn.hswstatic.com/gif/play/0b7f4e9b-f59c-4024-9f06-b3dc12850ab7-1920-1080.jpg"
+                        alt=""    
+                    />
+                ) : (
+                    <ul className="topList">
+                        <li className="topListItem">
+                            <Link className="link" to="/login">
+                                Login
+                            </Link>
+                        </li>
+                        <li className="topListItem">
+                            <Link className="link" to="/register">
+                                Register
+                            </Link>
+                        </li>
+                    </ul>
+                )
+            }
+            
             <i className="topSearchIcon fa-solid fa-magnifying-glass"></i>
         </div>
     </div>
